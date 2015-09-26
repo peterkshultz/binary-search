15544e67d9f101d0ccc2e8a2f9c6b09f2ec39771
peterkshultz <pshultz@umich.edu>
2015-09-26 18:46:24 -0400

    Add binary-search.cpp

---
+++ binary-search.cpp
@@ -0,0 +1,39 @@
+// Peter Shultz
+// EECS 281
+// hw1.cpp
+
+#include <vector>
+#include <math.h>
+bool binarySearch(const std::vector<int> &a_sorted_vector, int desired_search) {
+
+    int first = 0;
+    int last = static_cast<int>(a_sorted_vector.size() - 1);
+    int middle = 0;
+	
+    while (first <= last) {
+
+        middle = (first + last) / 2;
+        
+		if (sorted[middle] == desired_search) {
+	
+            return true;
+
+		}
+		
+		else if (sorted[middle] > desired_search){
+
+            last = middle - 1;
+
+		}
+        
+        else {
+            
+            first = middle + 1;
+            
+        }
+
+	}
+
+    return false;
+}
+
